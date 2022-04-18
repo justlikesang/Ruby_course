6c84110d8eeab6ad3e2327df6ca74680115e0a5d
Sang <sanghosuh@gmail.com>
2022-04-17 17:02:16 -0700

    learn puts print p method

---
+++ 01-Getting-Started/01-puts-method.rb
@@ -0,0 +1,23 @@
+# Used for printing in Ruby
+# Adds a line break after each 'put' statement
+# Does not coerce the data type like JS when printing '3' + 4
+
+puts "Hello world"
+puts "I'm alive and well!"
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 dollars or a profit of 25%!"
+
+puts 25 + 30
+
+hello = 'world'
+puts hello
+
+puts 4 + 3
+puts "4" + "3"
+
+puts
+puts
+puts
+
+puts "See there's a bunch of line breaks above"
---
+++ 01-Getting-Started/02-print-method.rb
@@ -0,0 +1,8 @@
+# print method does not create a line break like 'puts'
+
+puts "Hello"
+puts "world"
+
+print "Hello"
+print "world"
+print "There is no line break using 'print' statement"
