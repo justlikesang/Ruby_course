176f67488ae1f5a5eeedee7d14cfdb4ab20f8a7f
Sang <sanghosuh@gmail.com>
2022-04-17 17:46:44 -0700

    learn arithmetic rules

--- 01-Getting-Started/03-p-method.rb
+++ 01-Getting-Started/03-p-method.rb
@@ -1,1 +1,19 @@
-# 
+# 'p' method offers clarity of what the data type is or what is being used
+# p 'Steven Seagal'
+# // shows "Steven Seagal" in the log
+
+puts 'Steven Seagal'
+p 'Steven Seagal'
+
+puts 'Hi there, this is a
+big line break'
+# Hi there, this is a
+# big line break
+
+p 'Hi there, this is a
+big line break'
+# "Hi there, this is a\nbig line break"
+# notice how we get quotations and \n to account for new line
+
+puts 5
+p 5
---
+++ 01-Getting-Started/04-arithmetic-in-ruby.rb
@@ -0,0 +1,31 @@
+p 1 + 4
+
+p 10 - 6
+
+p 3 * 4
+
+p "School" + "bus"
+
+p 10 / 5
+
+# does not give floating numbers because floats are not
+# being used
+p 12 / 5 # logs 2
+
+# floats
+p 12.0 / 5 # logs 2.4
+p 12 / 5.0 # logs 2.4
+p 12.0 / 5.0 # logs 2.4
+
+# power of
+p 2 ** 5 #32
+p 3 ** 3 #27
+
+# modulo
+p 5 % 2 # logs 1
+p 16 % 4 # logs 0
+
+# bad habits in arithmetic
+# not putting 0 in front of .5 will cause error
+# p .5 / 2 # error
+p 0.5 / 2 # 0.25
